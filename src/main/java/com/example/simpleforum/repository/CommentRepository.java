ff63a643a430a8c08a8a32d468f46f0993e74a1b
ck.wong <ck3690324@gmail.com>
2026-06-24 10:27:02 +0900

    [mod] コメント関連修正

--- src/main/java/com/example/simpleforum/repository/CommentRepository.java
+++ src/main/java/com/example/simpleforum/repository/CommentRepository.java
@@ -1,21 +1,20 @@
 package com.example.simpleforum.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.simpleforum.model.Comments;
 
 public interface CommentRepository extends JpaRepository<Comments, Integer> {
+	Optional<Comments> findById(long id);
+	
+	/** 投稿ごとのコメント一覧取得 **/
+	List<Comments> findByPostIdOrderByCreatedAtAsc(Long postId);
 
-    /** 投稿ごとのコメント一覧取得 **/
-    List<Comments> findByPostIdOrderByCreatedAtAsc(Long postId);
-
-    /** ユーザーごとのコメント一覧取得 **/
-    List<Comments> findByUserId(Long userId);
-    
-    
-    
-   
-
+	/** ユーザーごとのコメント一覧取得 **/
+	List<Comments> findByUserId(Long userId);
+	
+	void deleteById(long id);
 }
