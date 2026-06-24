1aba168dd7884a15822145165ea8f6e894a409ab
ck.wong <ck3690324@gmail.com>
2026-06-24 13:40:04 +0900

    [mod] HTMLデザイン修正

--- src/main/java/com/example/simpleforum/model/Role.java
+++ src/main/java/com/example/simpleforum/model/Role.java
@@ -1,5 +1,16 @@
 package com.example.simpleforum.model;
 
 public enum Role {
-    USER, ADMIN
+	USER("一般"), 
+	ADMIN("管理者");
+
+	private final String label;
+
+	Role(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
 }
